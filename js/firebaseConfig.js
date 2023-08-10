1a04f80a64c5f74c5aa710d2664d706a3cace1b9
salman-attari <linksdisti@gmail.com>
2023-08-10 18:29:03 +0500

    First SMS

---
+++ js/firebaseConfig.js
@@ -0,0 +1,22 @@
+var config = {
+
+
+
+  apiKey: "AIzaSyA-ZedvYNPNBlQyQy26d5PhbrO-IaLR5qA",
+  authDomain: "signup-89c7a.firebaseapp.com",
+  databaseURL: "https://signup-89c7a-default-rtdb.firebaseio.com",
+  projectId: "signup-89c7a",
+  storageBucket: "signup-89c7a.appspot.com",
+  messagingSenderId: "987788007116",
+  appId: "1:987788007116:web:02719a692d3d319ce91469",
+  measurementId: "G-G3ZT9Y08KH"
+
+
+    // apiKey: "AIzaSyCiFb0LRM8yFDKBfN0ZEqPRmEadr7I4Jlo",
+    // authDomain: "playorbit-fbfaf.firebaseapp.com",
+    // databaseURL: "https://playorbit-fbfaf.firebaseio.com",
+    // projectId: "playorbit-fbfaf",
+    // storageBucket: "playorbit-fbfaf.appspot.com",
+    // messagingSenderId: "107154671391"
+  };
+  firebase.initializeApp(config);
